3b771a45432d8377a8925aa47ab629157a8b2664
Shinyoung Lee (Mark) <markshinyounglee@gmail.com>
2023-12-12 11:51:17 -0800

    Edited Dispatch.sv and ReservationStation.sv

--- ReservationStation.sv
+++ ReservationStation.sv
@@ -1,9 +1,29 @@
-module ReservationStation(
+module ReservationStation #(parameter ROB_ROW_COUNT = 64, parameter RS_ROW_COUNT = 64)(
 	input clk,
-	output reg[7:0] new_reservationTable[63:0] 
+	input rob, 
+	output reg[47:0] new_reservationTable[RS_ROW_COUNT-1:0], // 64 rows and each row has 1+7+6+6+1+6+1+12+2+6
+	output reg[45:0] rob[ROB_ROW_COUNT-1:0] // 64 rows and each row has 1+6+6+32+1=46
 );
 	
+	/*
+	pseudocode:
 	
+	for i = 0 to length(new_reservattionTable)-1
+	{
+		if src1 == (1 or -) and src == (1 or -) 
+		{
+			
+		}
+	}
 	
+	*/
+	integer i;
+	always@(posedge clk) 
+	begin
+		for(i = 0; i < 64; i++)
+		begin
+			
+		end
+	end
 	
 endmodule
